fa1c56d0baddd0ac74f52cc45d6ca551a125264d
KateOrlyk <kate.suprunenko24@gmail.com>
2022-07-20 21:25:11 +0300

    The heir of the Dog class.

---
+++ src/HomeAnimal/BigDog.java
@@ -0,0 +1,21 @@
+package HomeAnimal;
+
+public class BigDog extends Dog {
+    public BigDog(String name) {
+        super(name);
+    }
+
+    @Override
+    void greets() {
+        System.out.println("Woow");
+    }
+
+    @Override
+    void greets(Dog another) {
+        System.out.println("Woooow");
+    }
+
+    void greets(BigDog another) {
+        System.out.println("Woooooooow");
+    }
+}

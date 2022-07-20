320bbd86d68f4dfb5e78ad57f087848b608e5058
KateOrlyk <kate.suprunenko24@gmail.com>
2022-07-20 21:23:25 +0300

    Heir of the abstract class of Animals. Uses the Pet interface.

---
+++ src/HomeAnimal/Cat.java
@@ -0,0 +1,27 @@
+package HomeAnimal;
+
+public class Cat extends Animal implements Pet {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    void greets() {
+        System.out.println(super.getName() + " says Meow");
+    }
+
+    @Override
+    public void feed() {
+        System.out.println("Feeding " + super.getName());
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Walking " + super.getName());
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing with " + super.getName());
+    }
+}

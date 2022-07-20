3c9de19e73188f646422753b726f91a151bac6b3
KateOrlyk <kate.suprunenko24@gmail.com>
2022-07-20 21:24:04 +0300

    Heir of the abstract class of Animals. Uses the WildAnimal interface.

---
+++ src/HomeAnimal/Wolf.java
@@ -0,0 +1,17 @@
+package HomeAnimal;
+
+public class Wolf extends Animal implements WildAnimal {
+    public Wolf(String name) {
+        super(name);
+    }
+
+    @Override
+    void greets() {
+        System.out.println(super.getName() + " says Hawoo");
+    }
+
+    @Override
+    public void hunting() {
+        System.out.println(super.getName() + " goes hunting");
+    }
+}
